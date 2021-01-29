77183d5e54329ed8819e94f1ff552f7966078c19
alexnievesp7 <alejandro.nieves@patelhs.org>
2021-01-29 17:22:01 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+class Main {
+  public static void main(String[] args) {
+    
+    Scanner scan = new Scanner(System.in);
+
+    System.out.println("What is your favorite class?");
+    String name = new scan.next();
+
+    
+
+
+
+
+
+
+  }
+}
